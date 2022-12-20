d8130c2f4758e9df59c4dd464d55dbb09cf55775
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-12-20 10:52:21 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,12 +1,12 @@
-set env vars
-set -o allexport; source .env; set +o allexport;
+# set env vars
+# set -o allexport; source .env; set +o allexport;
 
-mkdir -p ./data
-mkdir -p ./castopod-media
-mkdir -p ./castopod-db
-mkdir -p ./castopod-cache
+# mkdir -p ./data
+# mkdir -p ./castopod-media
+# mkdir -p ./castopod-db
+# mkdir -p ./castopod-cache
 
-chown -R 444:444 ./data
-chown -R 444:444 ./castopod-media
-chown -R 444:444 ./castopod-db
-chown -R 444:444 ./castopod-cache
+# chown -R 444:444 ./data
+# chown -R 444:444 ./castopod-media
+# chown -R 444:444 ./castopod-db
+# chown -R 444:444 ./castopod-cache
