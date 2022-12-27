ed88328a401b10d52c59d98dd75f620bf00fbdf7
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-12-27 15:43:55 +0200

    readme

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,12 +1,9 @@
-# set env vars
-# set -o allexport; source .env; set +o allexport;
+set env vars
+set -o allexport; source .env; set +o allexport;
 
 mkdir -p ./data
 mkdir -p ./castopod-media
-# mkdir -p ./castopod-db
-# mkdir -p ./castopod-cache
+
 
 chown -R 1000:1000 ./data
 chown -R 1000:1000 ./castopod-media
-# chown -R 544:544 ./castopod-db
-# chown -R 544:544 ./castopod-cache
