93d71aab708288e892e5ce69234be2d1e171caea
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-12-29 10:12:21 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,45 +1,45 @@
-set env vars
-set -o allexport; source .env; set +o allexport;
+# set env vars
+# set -o allexport; source .env; set +o allexport;
 
-echo "Waiting...";
-sleep 20s;
+# echo "Waiting...";
+# sleep 20s;
 
-target=$(docker-compose port web-server 80)
-
-
-test1=$(curl http://${target}/cp-install \
-  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
-  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H 'upgrade-insecure-requests: 1' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
-  --compressed)
-
-echo "test1"
-echo $test1
-echo "test1"
+# target=$(docker-compose port web-server 80)
 
 
-test2=$(curl http://${target}/cp-install/create-superadmin \
-  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
-  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H 'cache-control: max-age=0' \
-  -H 'content-type: application/x-www-form-urlencoded' \
-  -H 'upgrade-insecure-requests: 1' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
-  --data-raw 'csrf_test_name=ccb8e9c2050b8839b467e617ec0c0f82cb9d5b8d87e0350950a821c131e483f5&username=root&email='${ADMIN_EMAIL}'&password='${ADMIN_PASSWORD}'' \
-  --compressed)
+# test1=$(curl http://${target}/cp-install \
+#   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+#   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+#   -H 'upgrade-insecure-requests: 1' \
+#   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
+#   --compressed)
 
-echo "test2"
-echo $test2
-echo "test2"
+# echo "test1"
+# echo $test1
+# echo "test1"
 
-  test3=$(curl http://${target}/cp-admin \
-  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
-  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H 'upgrade-insecure-requests: 1' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
-  --compressed)
 
-  echo "test3"
-echo $test3
-echo "test3"
+# test2=$(curl http://${target}/cp-install/create-superadmin \
+#   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+#   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+#   -H 'cache-control: max-age=0' \
+#   -H 'content-type: application/x-www-form-urlencoded' \
+#   -H 'upgrade-insecure-requests: 1' \
+#   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
+#   --data-raw 'csrf_test_name=ccb8e9c2050b8839b467e617ec0c0f82cb9d5b8d87e0350950a821c131e483f5&username=root&email='${ADMIN_EMAIL}'&password='${ADMIN_PASSWORD}'' \
+#   --compressed)
+
+# echo "test2"
+# echo $test2
+# echo "test2"
+
+#   test3=$(curl http://${target}/cp-admin \
+#   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+#   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+#   -H 'upgrade-insecure-requests: 1' \
+#   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
+#   --compressed)
+
+#   echo "test3"
+# echo $test3
+# echo "test3"
