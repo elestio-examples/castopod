c94f26e67f67f4fc1aa2ff2dab0f269f0306f42d
rpeshnegi <rupendrasinghnegi7@gmail.com>
2023-05-22 13:40:38 +0530

    updated run.sh and image name

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,4 @@
 cp docker/production/app/Dockerfile .
 sed -i "s~COPY castopod /opt/castopod~COPY ./ /opt/castopod~g" ./Dockerfile
-docker build . --tag elestio4test/traduora:latest;
+docker build . --tag elestio4test/castopod-app:latest;
 # TAG_TO_CHANGE
