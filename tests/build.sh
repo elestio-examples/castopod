fa9a3dba5fe3348f05934e249c1860efdefeb1d0
Muthiah131998 <110098347+Muthiah131998@users.noreply.github.com>
2023-07-12 21:11:50 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,3 @@
 cp docker/production/castopod/Dockerfile .
-sed -i 's/COPY castopod /COPY ./\t' ./Dockerfile
+sed -i 's/COPY castopod /COPY ./' ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/castopod-app:latest | docker load
