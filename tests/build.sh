538b4044e01c9f4d68fa910a97d352b3359b1460
Muthiah131998 <110098347+Muthiah131998@users.noreply.github.com>
2023-07-24 15:05:44 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,14 @@
 cp docker/production/castopod/Dockerfile .
-sed -i 's/COPY castopod /COPY .\/ /' ./Dockerfile
+mkdir ./castopod
+chmod +x ./castopod
+cp -rf ./app ./castopod
+cp -rf ./modules ./castopod
+cp -rf ./public ./castopod
+cp -rf ./themes ./castopod
+cp -rf ./vendor ./castopod
+cp -rf ./writable ./castopod
+cp -rf ./env.example ./castopod
+cp -rf LICENSE.md README.md ./castopod
+cp -rf ./spark ./castopod
+# sed -i 's/COPY castopod /COPY .\/ /' ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/castopod:latest | docker load
